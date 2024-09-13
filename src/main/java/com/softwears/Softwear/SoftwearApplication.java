881e151554c2131c640daa838f0ba6ba0a632f4c
Victor Olvera <victorolvera60@yahoo.com>
2024-09-13 14:09:31 -0500

    Init commit

---
+++ src/main/java/com/softwears/Softwear/SoftwearApplication.java
@@ -0,0 +1,13 @@
+package com.softwears.Softwear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwears/Softwear/SoftwearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwears.Softwear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
