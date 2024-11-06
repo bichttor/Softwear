e8f4132bc8cd9aae45143e30d45ea4353b9f732d
azaansundrani <azaansundrani@gmail.com>
2024-11-06 11:06:56 -0600

    Started on Cart.java

---
+++ src/main/java/com/softwears/Softwear/Orders/Cart.java
@@ -0,0 +1,97 @@
+package com.softwears.Softwear.Orders;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+/*Using the Entity tag creates a table with the values given so no schema will have to be written 
+ * You can essentially copy and paste this file for other data and just change the respective values
+*/
+
+@Entity
+@Table(name = "cart") /* set for data integrity */
+public class Cart {
+    /*@Columns are set for data integrity */
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "cart_price") 
+    private double cartPrice;
+
+    @Column(name = "cart_count") 
+    private int cartCount;
+
+    @OneToOne
+    @Column(name = "customer_id")
+    private int customerID;
+
+    @Column(name = "order_id")
+    private int orderID;
+
+    public Cart(int customerID){
+        this.customerID = customerID;
+        this.cartPrice = 0.00;
+        this.cartCount = 0;
+    }
+
+    public Cart(int customerID, double cartPrice, int cartCount){
+        this.customerID = customerID;
+        this.cartPrice = cartPrice;
+        this.cartCount = cartCount;
+    } 
+
+    public Cart(int customerID, double cartPrice, int cartCount, int orderID){
+        this.customerID = customerID;
+        this.cartPrice = cartPrice;
+        this.cartCount = cartCount;
+        this.orderID = orderID;
+    }
+
+    /* Getters & Setters */
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getCartPrice() {
+        return cartPrice;
+    }
+
+    public void setCartPrice(double cartPrice) {
+        this.cartPrice = cartPrice;
+    }
+
+    public int getCartCount() {
+        return cartCount;
+    }
+
+    public void setCartCount(int cartCount) {
+        this.cartCount = cartCount;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+}
