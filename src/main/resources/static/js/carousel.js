4c317957f75388f00e91ff3eadeb0732d2ee5693
Sofia Hernandez <hernandezsofia5g@gmail.com>
2024-11-11 16:25:35 -0600

    new slides

--- src/main/resources/static/js/carousel.js
+++ src/main/resources/static/js/carousel.js
@@ -1,35 +1,11 @@
-let currentSlideIndex = 0;
-const slides = document.querySelectorAll('.slide');
-const maxItemsPerSlide = 3;
-
-function showSlide(index) {
-    // Hide all slides first
-    slides.forEach((slide) => {
-        slide.style.display = 'none';
-    });
-
-    // Calculate start and end index for the visible items
-    const startIndex = (index + slides.length) % slides.length;
-    const endIndex = (startIndex + maxItemsPerSlide) % slides.length;
-
-    // Display the three items
-    for (let i = 0; i < maxItemsPerSlide; i++) {
-        const slideIndex = (startIndex + i) % slides.length;
-        slides[slideIndex].style.display = 'block';
-    }
-}
-
-function nextSlide() {
-    currentSlideIndex = (currentSlideIndex + maxItemsPerSlide) % slides.length;
-    showSlide(currentSlideIndex);
-}
-
-function prevSlide() {
-    currentSlideIndex = (currentSlideIndex - maxItemsPerSlide + slides.length) % slides.length;
-    showSlide(currentSlideIndex);
-}
-
-// Initial display of slides
-showSlide(currentSlideIndex);
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
 
 
