95c15f763ec426e828fb7326167b70adec311bc9
Sofia Hernandez <hernandezsofia5g@gmail.com>
2024-11-12 11:06:25 -0600

    Added more sliders

--- src/main/resources/static/js/carousel.js
+++ src/main/resources/static/js/carousel.js
@@ -6,6 +6,22 @@ $(document).ready(function() {
             $('#autoWidth').removeClass('cS-hidden');
         } 
     });  
+
+    $('#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth2').removeClass('cS-hidden');
+        } 
+    });
+
+    $('#autoWidth3').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth3').removeClass('cS-hidden');
+        } 
+    });
   });
 
 
