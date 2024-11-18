bc1eb6c198f6eb6ccd3eef800387f3627cbe4af9
mtzseb <martinezseb14@gmail.com>
2024-11-18 09:33:35 -0600

    Functioning Register Page
    Finished Sign Up Controller, allow back and forth between login and register page

--- src/main/java/com/softwears/Softwear/Users/SignUpController.java
+++ src/main/java/com/softwears/Softwear/Users/SignUpController.java
@@ -1,6 +1,17 @@
 package com.softwears.Softwear.Users;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class SignUpController {
 /*will link to sign up controller when the page is made and will handle its logic 
 i.e creating an account and storing it in the database */
+
+@GetMapping("/Register")
+public String showSignUp() {
+    return "Register";
+}
+
 }
