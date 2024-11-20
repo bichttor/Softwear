f4915679317f237c171d7d3de42d133866b5b6ff
mtzseb <martinezseb14@gmail.com>
2024-11-20 11:23:34 -0600

    working registration
    working register

--- src/main/java/com/softwears/Softwear/Users/SignUpController.java
+++ src/main/java/com/softwears/Softwear/Users/SignUpController.java
@@ -2,9 +2,10 @@ package com.softwears.Softwear.Users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -14,16 +15,21 @@ public class SignUpController {
 i.e creating an account and storing it in the database */
     @Autowired
     UsersService usersService;
-    @PostMapping("/sign-up")
-    public String postSignUp(@RequestParam(required = true) String first, 
+    @GetMapping("/Register")
+
+    public String showRegisterPage() {
+        return "Register"; 
+    }
+    
+    @PostMapping("/Register")
+        public String postSignUp(@RequestParam(required = true) String first, 
       @RequestParam(required = true) String last,
       @RequestParam(required = true) String  email,
       @RequestParam(required = true) String password,
-      @RequestParam(required = true) String phone,
-      Model model) {
+      @RequestParam(required = true) String phone) {
         Users newUser = new Users(first, last, email, phone, password, phone, null);
         usersService.addUser(newUser);
-        return "signup";
-    }
-    
+        return "Register";
+    } 
+
 }
