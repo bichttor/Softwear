9a2e9baaf628dace8403a4e89ab6a2cb9913cceb
Victor Olvera <victorolvera60@yahoo.com>
2024-11-22 16:14:42 -0600

    created sign up controller

--- src/main/java/com/softwears/Softwear/Users/SignUpController.java
+++ src/main/java/com/softwears/Softwear/Users/SignUpController.java
@@ -3,6 +3,7 @@ package com.softwears.Softwear.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,16 +15,21 @@ public class SignUpController {
 i.e creating an account and storing it in the database */
     @Autowired
     UsersService usersService;
-    @PostMapping("/sign-up")
+    @GetMapping("/register")
+    public String showSignUpPage() {
+        return "register";
+    }
+    @PostMapping("/register")
     public String postSignUp(@RequestParam(required = true) String first, 
       @RequestParam(required = true) String last,
       @RequestParam(required = true) String  email,
       @RequestParam(required = true) String password,
       @RequestParam(required = true) String phone,
       Model model) {
+        /*Creates new user */
         Users newUser = new Users(first, last, email, phone, password, phone, null);
         usersService.addUser(newUser);
-        return "signup";
+        return "register";
     }
     
 }
