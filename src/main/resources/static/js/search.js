b2aec3be7a25f2b6a47199ab48e56469f77fd933
Sofia Hernandez <hernandezsofia5g@gmail.com>
2024-12-01 20:00:25 -0600

    Search Bar design

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,4 +1,23 @@
+// Toggle Search Modal
+const searchToggle = document.getElementById('searchToggle');
+const searchModal = document.getElementById('searchModal');
+const closeSearch = document.getElementById('closeSearch');
 
+searchToggle.addEventListener('click', () => {
+    searchModal.classList.add('open');
+});
+
+closeSearch.addEventListener('click', () => {
+    searchModal.classList.remove('open');
+});
+
+// Close modal on outside click
+searchModal.addEventListener('click', (e) => {
+    if (e.target === searchModal) {
+        searchModal.classList.remove('open');
+    }
+});
+  
   let currentGender = 'all'; // Default to all
   let currentType = 'all'; // Default to all
   function navigate(gender,type) {
